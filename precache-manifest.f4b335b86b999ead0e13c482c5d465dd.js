31392d4d130c57d1ac1ca2983387da27b8468f6d
Gurp, Tijmen van <tijmen.van.gurp@philips.com>
2020-04-08 11:08:47 +0200

    Initial commit

---
+++ precache-manifest.f4b335b86b999ead0e13c482c5d465dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e5604b1495ff954dd8526fedc240b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "174251bd727caaaf6649",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "5e0acd9623d00bdd4e9d",
+    "url": "/static/css/main.906c6d42.chunk.css"
+  },
+  {
+    "revision": "174251bd727caaaf6649",
+    "url": "/static/js/2.20083621.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20083621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0acd9623d00bdd4e9d",
+    "url": "/static/js/main.7f9a098f.chunk.js"
+  },
+  {
+    "revision": "06f7e1edd7c54824b95a",
+    "url": "/static/js/runtime-main.16449d25.js"
+  },
+  {
+    "revision": "cd4c36ee11eb0b5d6b28be96ffe09f19",
+    "url": "/static/media/JetBrainsMono-Regular.cd4c36ee.woff2"
+  },
+  {
+    "revision": "e818b2e54806d99dca48daeab009409c",
+    "url": "/static/media/logo.e818b2e5.svg"
+  }
+]);
